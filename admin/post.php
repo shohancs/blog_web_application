53f00c347cf773baf6344f902ea2861935dc5678
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2023-07-28 21:25:34 +0600

    Post folder manage page done

--- admin/post.php
+++ admin/post.php
@@ -99,6 +99,43 @@
 																	?>
 														      </td>
 														      <td><?php echo $post_date; ?></td>
+														      <td>
+																	<div class="action-btn">
+																	  <ul>
+																	    <li>
+																	      <a href="post.php?do=Edit&u_id=<?php echo $post_id; ?>"><i class="fa-regular fa-pen-to-square edit"></i></a>
+																	    </li>
+																	    <li>
+																	      <a href="" data-bs-toggle="modal" data-bs-target="#postDel<?php echo $post_id; ?>"><i class="fa-regular fa-trash-can trush"></i></a>
+																	    </li>
+																	  </ul>
+																	</div>
+
+																	<!-- Modal Start -->
+																	<!-- ########## START: MODAL PART ########## -->
+										                        <div class="modal fade" id="postDel<?php echo $post_id; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+										                          <div class="modal-dialog">
+										                            <div class="modal-content">
+
+										                              <div class="modal-header">
+										                                <h1 class="modal-title fs-5" id="exampleModalLabel">Do You Sure?? To Move <i class="fa-regular fa-face-frown"></i><br> <span style="color: green;"><?php echo $title; ?></span> Trash folder!!</h1>
+
+										                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+										                              </div>
+
+										                              <div class="modal-body">
+										                                <div class="modal-btn">
+										                                  <a href="post.php?do=Trash&delPostId=<?php echo $post_id; ?>" class="btn btn-danger me-3">Trash</a>
+										                                  <a href="" class="btn btn-success" data-bs-dismiss="modal">Cancel</a>     
+										                                </div>
+										                              </div>
+
+										                            </div>
+										                          </div>
+										                        </div>
+										                        <!-- ########## END: MODAL PART ########## -->
+																	<!-- Modal End -->
+																</td>
 														    </tr>
 															<?php
 													}
