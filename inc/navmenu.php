389e799fd7bbf3b5de61376401c2fbaf1592f401
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2023-07-30 00:09:34 +0600

    Navmenu.php part done

--- inc/navmenu.php
+++ inc/navmenu.php
@@ -3,37 +3,57 @@
 		<ul class="nav nav-pills" id="mainNav">
 
 			<?php  
+
+				// Parent Cat
 				$sql = "SELECT cat_id AS 'pCat', cat_name AS 'pCatName' FROM category WHERE is_parent=0 AND status=1 ORDER BY cat_name ASC";
 				$readSql = mysqli_query($db, $sql);
 
 				while ( $row = mysqli_fetch_assoc($readSql) ) {
 					extract($row);
 
+					// Child Cat
 					$sql2 = "SELECT cat_id AS 'cCat', cat_name AS 'cCatName' FROM category WHERE is_parent='$pCat' AND status=1 ORDER BY cat_name ASC";
 					$readCSql = mysqli_query($db, $sql2);
 					$numOfChild = mysqli_num_rows($readCSql);
-					echo $numOfChild;
+
+					// ekhon drop down er bepar separ niye kaj korbo
+
+					// if ta dropdown chara gula print korebe
+					if ($numOfChild == 0) { ?>
+						<li class="dropdown">
+							<a class="dropdown-item dropdown-toggle" href="category.php?id=<?php echo $pCat; ?>">
+								<?php echo $pCatName; ?>
+							</a>
+						</li>
+					<?php }
+					
+					// dropdwn wala gula print hobe
+					else{ ?>
+						<li class="dropdown">
+							<a class="dropdown-item dropdown-toggle" href="category.php?id=<?php echo $pCat; ?>">
+								<?php echo $pCatName; ?>
+							</a>
+							<ul class="dropdown-menu">
+								<?php  
+									while( $row = mysqli_fetch_assoc($readCSql) ){
+										extract($row);
+										?>
+											<li><a class="dropdown-item" href="category.php?id=<?php echo $cCat; ?>"><?php echo $cCatName; ?></a></li>
+										<?php
+									}
+								?>
+								
+								
+							</ul>
+						</li>
+						
+					<?php }
 					
 				}
 			?>
 
-			<li class="dropdown">
-				<a class="dropdown-item dropdown-toggle" href="index.php">
-					Home
-				</a>
-			</li>
-			<li class="dropdown">
-				<a class="dropdown-item dropdown-toggle" href="#">
-					Shop
-				</a>
-				<ul class="dropdown-menu">
-					<li><a class="dropdown-item" href="shop-4-columns.html">4 Columns</a>
-					</li>
-					<li><a class="dropdown-item" href="shop-cart.html">Cart</a></li>
-					<li><a class="dropdown-item" href="shop-login.html">Login</a></li>
-					<li><a class="dropdown-item" href="shop-checkout.html">Checkout</a></li>
-				</ul>
-			</li>
+			
+			
 		</ul>
 	</nav>
 </div>
