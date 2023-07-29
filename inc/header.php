b757480b771738461aaca061c51fae33b2583508
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2023-07-29 15:17:15 +0600

    Logout page make

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,6 @@
 <?php  
 	ob_start();
+	session_start();
 	include "admin/inc/db.php";
 ?>
 
---
+++ logout.php
@@ -0,0 +1,15 @@
+<?php  
+
+	ob_start();
+
+	session_start();
+
+	session_unset();
+
+	session_destroy();
+
+	header("Location: index.php");
+
+	ob_end_flush();
+
+?>
