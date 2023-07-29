cbf4614266c59acabad3af1d234f64e93c8c92f3
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2023-07-29 23:52:35 +0600

    Now i am working for top navigation part

--- inc/navmenu.php
+++ inc/navmenu.php
@@ -1,6 +1,22 @@
 <div class="header-nav-main header-nav-main-square header-nav-main-effect-2 header-nav-main-sub-effect-1">
 	<nav class="collapse header-mobile-border-top">
 		<ul class="nav nav-pills" id="mainNav">
+
+			<?php  
+				$sql = "SELECT cat_id AS 'pCat', cat_name AS 'pCatName' FROM category WHERE is_parent=0 AND status=1 ORDER BY cat_name ASC";
+				$readSql = mysqli_query($db, $sql);
+
+				while ( $row = mysqli_fetch_assoc($readSql) ) {
+					extract($row);
+
+					$sql2 = "SELECT cat_id AS 'cCat', cat_name AS 'cCatName' FROM category WHERE is_parent='$pCat' AND status=1 ORDER BY cat_name ASC";
+					$readCSql = mysqli_query($db, $sql2);
+					$numOfChild = mysqli_num_rows($readCSql);
+					echo $numOfChild;
+					
+				}
+			?>
+
 			<li class="dropdown">
 				<a class="dropdown-item dropdown-toggle" href="index.php">
 					Home
