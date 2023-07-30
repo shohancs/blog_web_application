75f6677a697236b13ebeff8ccda7253be0a7988c
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2023-07-30 11:52:28 +0600

    Recent Side bar done

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -9,6 +9,7 @@
 		</div>
 	</form>
 
+	<!-- ########## START: RIGHT SIDE BAR ########## -->
 	<h5 class="font-weight-bold pt-4">Categories</h5>
 	<ul class="nav nav-list flex-column mb-5">
 		<?php  
@@ -79,45 +80,18 @@
 					
 				}
 			?>
-		
-		
 	</ul>
+	<!-- ########## END: RIGHT SIDE BAR ########## -->
 
 	<div class="tabs tabs-dark mb-4 pb-2">
 		<ul class="nav nav-tabs">
 			<li class="nav-item active">
-				<a class="nav-link show active text-1 font-weight-bold text-uppercase" href="#popularPosts" data-toggle="tab">Popular</a>
-			</li>
-
-			<li class="nav-item">
-				<a class="nav-link text-1 font-weight-bold text-uppercase" href="#recentPosts" data-toggle="tab">Recent</a>
+				<a class="nav-link show active text-1 font-weight-bold text-uppercase" href="#recentPosts" data-toggle="tab">Recent</a>
 			</li>
 		</ul>
 		<div class="tab-content">
-			<!-- Popular Post -->
-			<div class="tab-pane active" id="popularPosts">
-				<ul class="simple-post-list">
-					<li>
-						<div class="post-image">
-							<div class="img-thumbnail img-thumbnail-no-borders d-block">
-								<a href="blog-post.html">
-									<img src="assets/img/blog/square/blog-11.jpg" width="50" height="50" alt="">
-								</a>
-							</div>
-						</div>
-						<div class="post-info">
-							<a href="blog-post.html">Nullam Vitae Nibh Un Odiosters</a>
-							<div class="post-meta">
-								 Nov 10, 2020
-							</div>
-						</div>
-					</li>
-				</ul>
-			</div>
-			<!-- Popular Post -->
-
 			<!-- Recent Post -->
-			<div class="tab-pane" id="recentPosts">
+			<div class="tab-pane active" id="recentPosts">
 				<ul class="simple-post-list">
 					<?php  
 						$recentSql = "SELECT * FROM post WHERE status=1 ORDER BY post_id DESC LIMIT 3";
